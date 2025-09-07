d016498ac8791fddbdb25039dd842280e7263f9e
koeugeneko <koeugeneko@gmail.com>
2025-09-07 19:34:37 +0100

    Cleared everything, planning to add login page next

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,63 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            buttonEnter = new Button();
-            txtName = new TextBox();
-            labelName = new Label();
             SuspendLayout();
-            // 
-            // buttonEnter
-            // 
-            buttonEnter.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
-            buttonEnter.Location = new Point(390, 247);
-            buttonEnter.Name = "buttonEnter";
-            buttonEnter.Size = new Size(453, 64);
-            buttonEnter.TabIndex = 0;
-            buttonEnter.Text = "Enter";
-            buttonEnter.UseVisualStyleBackColor = true;
-            buttonEnter.Click += buttonEnter_Click;
-            // 
-            // txtName
-            // 
-            txtName.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
-            txtName.Location = new Point(445, 203);
-            txtName.Name = "txtName";
-            txtName.Size = new Size(398, 27);
-            txtName.TabIndex = 1;
-            txtName.Text = "Enter Name";
-            txtName.Click += textName_click;
-            txtName.TextChanged += txtName_TextChanged;
-            // 
-            // labelName
-            // 
-            labelName.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
-            labelName.AutoSize = true;
-            labelName.Location = new Point(390, 206);
-            labelName.Name = "labelName";
-            labelName.Size = new Size(49, 20);
-            labelName.TabIndex = 2;
-            labelName.Text = "Name";
-            labelName.Click += labelName_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1262, 673);
-            Controls.Add(labelName);
-            Controls.Add(txtName);
-            Controls.Add(buttonEnter);
             Name = "Form1";
             Text = "WinForms";
             Load += Form1_Load;
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
-
-        private Button buttonEnter;
-        private TextBox txtName;
-        private Label labelName;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -13,37 +13,5 @@ namespace TimeTable
         {
 
         }
-
-        private void buttonEnter_Click(object sender, EventArgs e)
-        {
-            string name = txtName.Text;
-            if (!string.IsNullOrWhiteSpace(name) && name != "Enter Name")
-            {
-                MessageBox.Show($"Welcome, {name}!", "Greeting", MessageBoxButtons.OK, MessageBoxIcon.Information);
-            }
-            else
-            {
-                MessageBox.Show("Please enter a valid name.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
-        }
-
-        private void txtName_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void textName_click(object sender, EventArgs e)
-        {
-            if (txtName.Text == "Enter Name")
-            {
-                txtName.Text = "";
-            }
-        }
-
-
-        private void labelName_Click(object sender, EventArgs e)
-        {
-
-        }
     }
 }
