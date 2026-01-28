744c4dbd43cd27dac532063b90a845b52e351ffe
ranyoucame <eugeneko@exe-coll.ac.uk>
2026-01-28 09:36:14 +0000

    added sidebar script

---
+++ UI/SideBar.Designer.cs
@@ -0,0 +1,44 @@
+﻿namespace TimeTable.UI
+{
+    partial class SideBar
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            SuspendLayout();
+            // 
+            // SideBar
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Name = "SideBar";
+            Size = new Size(951, 593);
+            ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
